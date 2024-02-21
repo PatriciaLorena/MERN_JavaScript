e00f50c464afdf9fdc1ef16257660fd621c85304
Paty <patrilorenfer@gmail.com>
2024-02-21 08:39:48 -0300

    cambios a la clase  ninja

--- ninja.js
+++ ninja.js
@@ -16,24 +16,6 @@ class Ninja{
     }
 }
 
-class Ninja{
-    constructor(nombre, salud, velocidad, fuerza){
-        this.nombre = nombre;
-        this.salud = salud;
-        this.velocidad = 3;
-        this.fuerza = 3;
-    }
-    sayName(){
-        console.log(`El nombre del ninja es: ${ this.nombre} `);
-    }
-    showStats(){
-        console.log(`el nombre del nimja es: ${this.nombre} tiene una saldud de ${this.salud} su velocidad es: ${this.velocidad} con una fuerza de ${this.fuerza}`);
-    }
-    drinSake(){
-        this.fuerza +=3;
-    }
-}
-
 const ninja1 = new Ninja("Hyabusa","medio");
 ninja1.sayName();
 ninja1.drinSake();
